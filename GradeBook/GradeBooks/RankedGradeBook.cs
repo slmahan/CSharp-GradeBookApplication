20c8e38df5a056ce9b68c7e7ff0b8d5d850c3d6e
Bueller <scott.mahan@gmail.com>
2021-02-16 08:29:27 -0500

    Module Complete
    Module Complete

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = Enums.GradebookType.Ranked;
+            Type = Enums.GradeBookType.Ranked;
         }
     }
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Type = Enums.GradebookType.Standard;
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
