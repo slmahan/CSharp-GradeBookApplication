2c8cbb1a59544781a20c8cfb57a5e768930dba30
Bueller <scott.mahan@gmail.com>
2021-02-23 16:15:47 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -41,6 +41,17 @@ namespace GradeBook.GradeBooks
                 return;
             }
             base.CalculateStatistics();
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
