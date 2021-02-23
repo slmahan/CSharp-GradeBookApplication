49d2644ea612c3a216ab2e17b8b40e0c81f04d44
Bueller <scott.mahan@gmail.com>
2021-02-23 16:07:30 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,7 +18,17 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Must have at least 5 students for ranked grading");
             }
-         
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            if (averageGrade >= grades[(threshold * 2) - 1])
+                return 'B';
+            if (averageGrade >= grades[(threshold * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(threshold * 4) - 1])
+                return 'D';
             return 'F';
         }
     }
