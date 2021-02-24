7d4bbad4d77e20a302de52031a50d45320708bb5
Bueller <scott.mahan@gmail.com>
2021-02-24 10:33:06 -0500

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -52,7 +52,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine("{0} is not a asupported type of gradebook, please try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
